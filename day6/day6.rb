0a79231b1c5a75084cbcb60b1e3c2f098649f64e
Seth Geoghegan <seth.geoghegan@gmail.com>
2020-12-06 10:09:30 -0800

    nevermind, comments were ugly

--- day6/day6.rb
+++ day6/day6.rb
@@ -3,14 +3,11 @@
 # [["abc"], ["a", "b", "c"], ["ab", "ac"], ["a", "a", "a", "a"], ["b"]]
 data = File.read("input1.txt").split("\n\n")
 
-# "ab\nac" => ["abac"] => ["a","b","a","c"].uniq => ["a","b","c"].length = 3
 sum = data.map {|g| g.gsub(/\n/, "").split("").uniq.length }.reduce(:+)
 puts "Part 1 solution: #{sum}"
 
 yesCount = 0
-# break the file input into groups of answers
 data.each do |group|
-  # "ab\nac" => ["ab","ac"] => [["a","b"],["a","c"]] => ["a","b"] & ["a","c"] => ["a"].length
   yesCount += group.split("\n").map{|g| g.split("")}.reduce(:&).length
 end
 
