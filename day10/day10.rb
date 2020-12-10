a885c1877e71b47ff15f506d86b1a33e048d3654
Seth Geoghegan <seth.geoghegan@gmail.com>
2020-12-10 13:15:44 -0800

    Day 10

---
+++ day10/day10.rb
@@ -0,0 +1,26 @@
+# https://adventofcode.com/2020/day/10
+
+adapters = File.readlines('input.txt').collect(&:to_i).sort
+
+
+# PART 1
+my_device = adapters.last + 3
+outlet = 0
+diff_arr = [outlet, *adapters, my_device].each_cons(2).map{ |a,b| b-a }
+puts "Part 1 answer is #{diff_arr.count(1) * diff_arr.count(3)}"
+
+# ## PART 2
+# {adapter => number of paths to adapter}
+num_paths_to = Hash.new(0)
+# start with only one path to the outlet
+num_paths_to[outlet] = 1
+
+[outlet,*adapters].each do |adapter|
+  [1,2,3].each do |hop|
+    if ([*adapters,my_device].include?(adapter+hop))
+      num_paths_to[adapter+hop] += num_paths_to[adapter]
+    end
+  end
+end
+
+puts "Part 2 answer is #{num_paths_to[my_device]}"
