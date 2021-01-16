4b574f66e1b75d94aeab1179236057f71e2d899b
Seth Geoghegan <seth.geoghegan@gmail.com>
2021-01-16 09:34:48 -0800

    Update day1.rb

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,27 +1,21 @@
 # https://adventofcode.com/2020/day/1
-
 def pairSumTo(arr1, num)
-  arr2 = arr1.map { |i| num - i  }
+  arr2 = arr1.map {|i| num - i}
   arr1 & arr2
+end
+
+def sumTo(arr, num, n)
+  arr.combination(n).each {|a| return a if a.sum == num }
 end
 
 data = File.readlines('input1.txt').collect! &:to_i
 
 # part 1
-answer = pairSumTo(data,2020)
-puts "#{answer[0]} + #{answer[1]} = #{answer[0]+answer[1]}"
-puts "#{answer[0]} * #{answer[1]} = #{answer[0]*answer[1]}"
+answer = sumTo(data,2020,2)
+puts "#{answer[0]} + #{answer[1]} = #{answer.sum}"
+puts "#{answer[0]} * #{answer[1]} = #{answer.reduce(&:*)}"
 
-# # part 2
-for i in 0..data.length - 1
-  for j in i..data.length - 1
-    for k in j..data.length - 1
-      if data[i] + data[j] + data[k] == 2020
-        product = data[i] * data[j] * data[k]
-        puts "#{data[i]} + #{data[j]} + #{data[k]}= 2020"
-        puts "#{data[i]} * #{data[j]} * #{data[k]}= #{product}"
-        break;
-      end
-    end
-  end
-end
+# part 2
+answer = sumTo(data,2020,3)
+puts "#{answer[0]} + #{answer[1]} = #{answer.sum}"
+puts "#{answer[0]} * #{answer[1]} = #{answer.reduce(&:*)}"
